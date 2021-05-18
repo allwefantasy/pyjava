f6f10700c9e2a6b39687a664d036690d8ea6d760
WilliamZhu <allwefantasy@gmail.com>
2021-05-18 22:45:49 +0800

    update

--- dev/release.sh
+++ dev/release.sh
@@ -1,3 +1,1 @@
-#git co mslql-2.4
-git co mlsql-3.0
-mvn clean deploy -DskipTests -Pdisable-java8-doclint -Prelease-sign-artifacts
+mvn clean install -DskipTests -Pdisable-java8-doclint -Prelease-sign-artifacts
