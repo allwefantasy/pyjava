92930c53164471fb45f8081fac7cfd014125379a
WilliamZhu <allwefantasy@gmail.com>
2021-09-02 20:25:38 +0800

    add release script

--- dev/release.sh
+++ dev/release.sh
@@ -1,1 +1,29 @@
-mvn clean install -DskipTests -Pdisable-java8-doclint -Prelease-sign-artifacts
+#!/usr/bin/env bash
+
+version=${1:-0.3.3}
+
+quoteVersion=$(cat python/pyjava/version.py|grep "__version__" |awk -F'=' '{print $2}'| xargs )
+
+if [[ "${version}" != "${quoteVersion}" ]];then
+   echo "version[${quoteVersion}] in python/pyjava/version.py is not match with version[${version}] you specified"
+   exit 1
+fi
+
+if [[ ! -d '.repo' ]];then
+   echo "Make sure this script executed in root directory of pyjava"
+   exit 1
+fi
+
+echo "deploy pyjava jar based on spark243...."
+mlsql_plugin_tool spark243
+mvn clean deploy -DskipTests -Pdisable-java8-doclint -Prelease-sign-artifacts
+
+echo "deploy pyjava jar based on spark311...."
+mlsql_plugin_tool spark311
+mvn clean deploy -DskipTests -Pdisable-java8-doclint -Prelease-sign-artifacts
+
+echo "deploy pyjava pip...."
+cd python
+rm -rf dist
+pip uninstall -y pyjava && python setup.py sdist bdist_wheel && cd ./dist/ && pip install pyjava-${version}-py3-none-any.whl && cd -
+twine upload dist/*
--- python/sbin/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-pip uninstall -y pyjava && python setup.py sdist bdist_wheel && cd ./dist/ && pip install pyjava-0.3.2-py3-none-any.whl && cd -
-twine upload dist/*
